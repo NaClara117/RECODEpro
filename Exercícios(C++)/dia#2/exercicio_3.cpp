82859698d4663891840b869b4967c51232435303
AnaXP <analves.ax@gmail.com>
2023-07-03 13:43:41 -0300

    renomeação

--- Exercícios(C++)/dia#2/exercicio_3.cpp
+++ Exercícios(C++)/dia#2/exercicio_3.cpp
@@ -1,39 +1,3 @@
-// Algoritmo "Atividade 3"
-//     // Disciplina: Computacao e Logica de Programacao
-//     // Professor: Flávio Mota da Cruz
-//     // Autor(a): Ana Clara Alves
-//     // Data: 30/06/2023
-// Var
-//     // Declaracoes das variaveis
-//     nome: caractere
-//     valor, desconto: real
-
-// Inicio
-//     // Comandos, procedimento, funcoes, operadores, etc...
-//     escreval("Qual seu nome?")
-//     leia(nome)
-//     escreval("Qual o valor da compra?")
-//     leia(valor)
-//     se (valor >= 0) entao
-//         se (valor >= 500) entao
-//         desconto<-0.2
-//         senao
-//             se ((valor >= 200) e (valor < 500)) entao
-//             desconto<-0.15
-
-//             senao
-//                 se (valor < 200) entao
-//                 desconto<-0.1
-//                 fimse
-//             fimse
-//         fimse
-//     desconto <- valor*desconto
-//     valor<- valor-desconto))
-//     escreval (nome, ", o desconto é de R$", desconto, " e a compra ficará em R$", valor, "." )
-//     senao
-//             escreval("VALOR INVÁLIDO, TENTE NOVAMENTE")
-//     fimse
-// Fimalgoritmo
 // Exercício 3
 // Disciplina: Computacao e Logica de Programacao
 // Professor: Flávio Mota da Cruz
@@ -47,22 +11,30 @@
 int main()
 {
    setlocale(LC_ALL, "Portuguese");
-   int dia;
-   printf("Qual dia deseja pesquisar?\n");
-   printf("1 - DOMINGO\n");
-   printf("2 - SEGUNDA\n");
-   printf("3 - TERCA\n");
-   printf("4 - QUARTA\n");
-   printf("5 - QUINTA\n");
-   printf("6 - SEXTA\n");
-   printf("7 - SABADO\n");
-   scanf("%d", &dia);
-   if ((dia == 2) || (dia == 3)|| (dia == 4)|| (dia == 5)|| (dia == 6))
+   float valor, desconto;
+   printf("Qual o valor da compra? ");
+   scanf("%f", &valor);
+   if (valor >= 0)
    {
-      printf("Dia de TRABALHO");
+      if (valor >= 500)
+      {
+         desconto=0.2;
+      }
+      if ((valor >= 200) && (valor < 500))
+      {
+         desconto=0.15;
+      }
+      if (valor < 200)
+      {
+         desconto=0.1;
+      }   
+   desconto = valor*desconto;
+   valor = valor-desconto;
+   printf("Cliente, o desconto e de R$%f e a compra ficara em R$%f.", desconto, valor);
    }
-   if ((dia == 1) || (dia == 7))
+   else
    {
-      printf("DIA DE DESCANSO");
+      printf("VALOR INVALIDO, TENTE NOVAMENTE");
    }
+
 }
