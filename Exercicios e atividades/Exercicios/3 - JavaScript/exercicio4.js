b385109c4bd9134a29af5b3742d7e38e2a6225d1
AnaXP <analves.ax@gmail.com>
2023-07-03 10:13:22 -0300

    Renomeação de arquivos

--- Exercicios e atividades/Exercicios/3 - JavaScript/exercicio4.js
+++
@@ -1,6 +0,0 @@
-var salario, reajuste;
-    salario = prompt("Qual o salário do funcionário?");
-    reajuste = prompt("Qual a taxa de reajuste do salário?");
-    salario=salario*(1+(reajuste/100));
-    alert("O salário será de R$" + salario + " com o rejuste anual de " + reajuste + "% no primeiro ano.");
-
---
+++ Exercícios/README.md
@@ -0,0 +1,2 @@
+# Atividades
+Atividades realizadas em aula durante o projeto da RECODEpro
